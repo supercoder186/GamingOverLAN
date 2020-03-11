45d044edf8c02ed358c8f3fc101d1f9af251aad5
supercoder186 <aadit.kolar@gmail.com>
2020-03-11 14:06:06 +0530

    Added client

---
+++ client.py
@@ -0,0 +1,18 @@
+from tanktroublemoment import Server
+import pyautogui
+
+host = '192.168.137.1'
+port = 8080
+server = Server(host,port,1,"server")
+server.send("client","hi")
+
+while True:
+    data = server.get('all')
+    if data:
+        for objects in data:
+            obj = objects[1]
+            if obj[1] == 'n':
+                pyautogui.keyUp(obj[0]);
+            else:
+                pyautogui.keyDown(obj[0]);
+    
