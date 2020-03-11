e2ad2dba7f02103d99642d4e54827f36ac4836c1
Mugilan Ganesan <49064156+MugilanGN@users.noreply.github.com>
2020-03-11 14:08:27 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,18 +1,40 @@
-from UltraSockets import Server
-import pyautogui
+from pynput.keyboard import Key, Listener
+import time
+from UltraSockets import Client
 
 host = '192.168.137.1'
 port = 8080
-server = Server(host,port,1,"server")
-server.send("client","hi")
+name = 'client'
 
-while True:
-    data = server.get('all')
-    if data:
-        for objects in data:
-            obj = objects[1]
-            if obj[1] == 'n':
-                pyautogui.keyUp(obj[0]);
-            else:
-                pyautogui.keyDown(obj[0]);
-    
+pc = Client(host, port, name)
+time.sleep(1)
+
+print(pc.get('all')[0][1])
+print("Connection established")
+
+chars = [char for char in 'qesdf']
+
+pressedChars = []
+for char in chars:
+    pressedChars.append([char, False])
+
+print(pressedChars)
+
+def on_press(k):
+    key = str(k).replace("'", "")
+    if key in chars and not pressedChars[chars.index(key)][1]:
+        pc.send("server", key + "y")
+        pressedChars[chars.index(key)][1] = True
+
+
+def on_release(k):
+    key = str(k).replace("'", "")
+    if key in chars:
+        pc.send("server", key + "n")      
+        pressedChars[chars.index(key)][1] = False
+
+
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
