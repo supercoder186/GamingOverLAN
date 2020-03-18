41cc413b2dbfb50cd5dce7cb559d2c2fa48ee066
Aadit Kolar <aadit.kolar@gmail.com>
2020-03-18 15:09:13 +0530

    Added queue, still buggy

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import tkinter as tk
+from queue import Queue
 from threading import Thread
 
 import pyautogui
@@ -17,15 +18,14 @@ running = False
 thread = None
 server = None
 host_ip = get_ip_address()
+q = Queue(maxsize=3)
 
 
 def run_server(host, chars):
-    print("Starting server...")
-    global server, running
+    global server, q
     server = Server(host, 1, 'server')
     server.send('client', chars)
-    print("Server running...")
-    while running:
+    while not q.qsize():
         data = server.get('all')
         if data:
             for objects in data:
@@ -39,8 +39,7 @@ def run_server(host, chars):
 
 
 def toggle_host():
-    global tSelectHost, tSelectChar, running, thread, host_ip
-    print("Button pressed")
+    global tSelectHost, tSelectChar, running, thread, host_ip, btn_text
     running = not running
     print('Running:', running)
     if running:
@@ -50,7 +49,10 @@ def toggle_host():
         chars = tSelectChar.get('1.0', 'end-1c')
         thread = Thread(target=run_server, args=[host, chars])
         thread.start()
+        btn_text.set('Stop server')
     else:
+        btn_text.set('Start server')
+        q.put('stop')
         thread.join()
 
 
@@ -73,7 +75,8 @@ lCharHint.place(height=32, relwidth=0.2, y=104)
 tSelectChar = tk.Text(root, padx=5, pady=4)
 tSelectChar.place(height=32, y=104, relwidth=0.7, relx=0.25)
 
-btServerToggle = tk.Button(root, text='Start Host', padx=10, pady=5, command=toggle_host)
+btn_text = tk.StringVar(value='Start Server')
+btServerToggle = tk.Button(root, textvariable=btn_text, padx=10, pady=5, command=toggle_host)
 btServerToggle.place(height=32, y=144, relwidth=0.2, relx=0.4)
 
 root.mainloop()
