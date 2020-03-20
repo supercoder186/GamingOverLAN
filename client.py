42030a314a6a935337e62477137ff0cd3a926702
Aadit Kolar <aadit.kolar@gmail.com>
2020-03-20 15:15:11 +0530

    Added comments to client.py

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ from pynput.keyboard import Listener
 
 from UltraSockets import Client
 
+# declare and assign all global variables
 c = None
 pc = None
 connected = False
@@ -14,87 +15,94 @@ pressedChars = None
 listener = None
 
 
+# define the functions for when a button is pressed or released
 def on_press(k):
     global pressedChars, pc
-    key = str(k).replace("'", "")
+    key = str(k).replace("'", "")  # remove the surrounding quotes
+    # checks if the pressed key is in the allowed character list, taking rebinds into account
     if key in rebinds and not pressedChars[rebinds.index(key)][1]:
-        pc.send("server", chars[rebinds.index(key)] + "y")
-        pressedChars[rebinds.index(key)][1] = True
+        pc.send("server", chars[rebinds.index(key)] + "y")  # tell server that key has been pressed
+        pressedChars[rebinds.index(key)][1] = True  # store that the key has been pressed
 
 
 def on_release(k):
     global pressedChars, pc
-    key = str(k).replace("'", "")
+    key = str(k).replace("'", "")  # remove the surrounding quotes
+    # checks if the pressed key is in the allowed character list, taking rebinds into account
     if key in rebinds:
-        pc.send("server", chars[rebinds.index(key)] + "n")
-        pressedChars[rebinds.index(key)][1] = False
+        pc.send("server", chars[rebinds.index(key)] + "n")  # tell server that key has been released
+        pressedChars[rebinds.index(key)][1] = False  # store that the key has been released
 
 
 def start_client(host):
     global c, pc, char_txt, client_btn_txt, connected, chars, rebinds, pressedChars, listener
     try:
-        pc = Client(host, 'client')
+        pc = Client(host, 'client')  # start client and connect to host
     except TypeError as e:
-        print(e)
+        # this is raised if the format of the host is incorrect. Use the format <ip>:<port>
         connected = False
         return
     except ConnectionRefusedError as e:
         print(e)
-        connected = False
+        connected = False  # if connection is refused, set connected to False
         return
-    client_btn_txt.set('Disconnect')
+    client_btn_txt.set('Disconnect')  # change text on button
+
+    # wait until the server sends the character list
     obj = None
     while obj is None:
         time.sleep(0.1)
         obj = pc.get('all')
 
-    c = obj[0][1]
+    c = obj[0][1]  # retrieves the character list from the sent data
 
     char_txt.set('Character string: ' + c)
 
-    chars = [char for char in c]
+    chars = [char for char in c]  # turns the character string into a list
     rebinds = chars
 
     pressedChars = []
     for char in rebinds:
-        pressedChars.append([char, False])
+        pressedChars.append([char, False])  # creates the list of pressed characters. All are False in the beginning
 
-    listener = Listener(on_press=on_press, on_release=on_release)
-    listener.start()
+    listener = Listener(on_press=on_press, on_release=on_release)  # define the key press listener
+    listener.start()  # start the key press listener
 
 
 def stop_client():
     global client_btn_txt, listener
-    pc.close()
-    client_btn_txt.set('Connect')
-    char_txt.set('Character string: ')
-    listener.stop()
+    pc.close()  # notify the server that the client is leaving
+    client_btn_txt.set('Connect')  # change the client button text
+    char_txt.set('Character string: ')  # change the character string display text
+    listener.stop()  # stop the key listener
 
 
 def toggle_client():
     global connected, tHost
-    connected = not connected
-    if connected:
-        start_client(tHost.get('1.0', 'end-1c'))
+    connected = not connected  # toggle connected
+    if connected:  # check if the client has an active connection
+        start_client(tHost.get('1.0', 'end-1c'))  # start the client
     else:
-        stop_client()
+        stop_client()  # stop the client
 
 
 def update_rebinds():
     global rebinds, tRebinds, c, chars, pc, pressedChars
-    pc.send('server', 'release_all')
-    r = tRebinds.get('1.0', 'end-1c')
-    if (not r) or not (len(r) == len(c)):
+    pc.send('server', 'release_all')  # tell the server to release all keys
+    r = tRebinds.get('1.0', 'end-1c')  # retrieve the rebind string from the text field
+    if (not r) or not (len(r) == len(c)):  # checks if the rebind string is the right length
         print('Invalid rebind string!')
-        rebinds = chars
+        rebinds = chars  # disable rebinds by setting all rebinds to their respective characters
     else:
-        rebinds = [rebind for rebind in r]
+        rebinds = [rebind for rebind in r]  # change the rebind list
 
+    # reassign pressedChars based on the new rebind string
     pressedChars = []
     for char in rebinds:
         pressedChars.append([char, False])
 
 
+# Define the layout
 root = tk.Tk()
 root.title('GamingOverLAN Client')
 canvas = tk.Canvas(root, width=480, height=200)
@@ -123,4 +131,5 @@ tRebinds.place(relwidth=0.75, relx=0.2, height=32, y=120)
 btUpdateRebinds = tk.Button(root, text='Update rebinds', command=update_rebinds)
 btUpdateRebinds.place(relwidth=0.8, height=32, y=160, relx=0.1)
 
+# Start the UI loop
 root.mainloop()
